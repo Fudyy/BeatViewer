eded3bc475847bb25f9f11237c570bee7dabd2c1
Carlos González <104286054+Fudyy@users.noreply.github.com>
2024-05-26 19:29:44 -0400

    Add: ossapi initial settings

--- src/main.py
+++ src/main.py
@@ -4,12 +4,25 @@ from dotenv import load_dotenv
 from disnake.ext import commands
 from logger.logger import logger
 from platform import python_version
+import ossapi
+from sys import exit
 
 load_dotenv()
 
 TOKEN = environ["TOKEN"]
+OSU_ID = environ["OSU-ID"]
+OSU_SECRET = environ["OSU-SECRET"]
+
+if TOKEN is None:
+    logger.error("No discord token provided on environment variables.")
+    exit()
+
+if OSU_ID is None or OSU_SECRET is None:
+    logger.error("No osu oauth credentials provided on environment variables.")
+    exit()
 
 bot = commands.InteractionBot()
+osu = ossapi.OssapiAsync(OSU_ID, OSU_SECRET, scopes=[ossapi.Scope.PUBLIC])
 
 @bot.event
 async def on_ready():

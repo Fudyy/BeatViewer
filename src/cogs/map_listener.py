033e3d57949847f082b902fb2d00b02bba20fddf
Carlos González <104286054+Fudyy@users.noreply.github.com>
2024-05-28 02:14:21 -0400

    Refactor: reverse validation on MapListener

--- src/cogs/map_listener.py
+++ src/cogs/map_listener.py
@@ -18,39 +18,41 @@ class MapListener(commands.Cog):
         # search if the message have a beatmap link in it.
         match = re.search(self.map_regex, message.content)
         
-        if match:
-            set_id = match.group(1)
-            map_id = match.group(2) if match.group(2) else None
+        if not match:
+            return
+            
+        set_id = match.group(1)
+        map_id = match.group(2) if match.group(2) else None
 
-            # if the link have an specified diff it shows it
-            # example: https://osu.ppy.sh/beatmapsets/24313#osu/104229
-            if map_id:
-                try:
-                    # it uses the beatmapset endpoit so it can get the language and genre info.
-                    mapset = await osu.beatmapset(set_id)
-                    beatmap = next((bm for bm in mapset.beatmaps if bm.id == int(map_id)), None)
-                    # prevent to show a difficulty that doesn't exists (yeah i know that more than someone is gonna hard test the bot)
-                    if beatmap:
-                        logger.info(f"Sending map info:  MAP ID: {beatmap.id} | CHANNEL: {message.channel.id}")
-                        try:
-                            await message.reply(embed=map_info.create(beatmap, mapset))
-                        except Exception as e:
-                            logger.error(f"There was an error sending the map info: {e}")
-                except:
-                    pass
-            # if the link only have the mapset id it uses the highest difficulty to show up
-            # example: https://osu.ppy.sh/beatmapsets/24313
-            else:
-                try:
-                    mapset = await osu.beatmapset(beatmapset_id=set_id)
-                    beatmap = max(mapset.beatmaps, key=lambda b: b.difficulty_rating)
+        # if the link have an specified diff it shows it
+        # example: https://osu.ppy.sh/beatmapsets/24313#osu/104229
+        if map_id:
+            try:
+                # it uses the beatmapset endpoit so it can get the language and genre info.
+                mapset = await osu.beatmapset(set_id)
+                beatmap = next((bm for bm in mapset.beatmaps if bm.id == int(map_id)), None)
+                # prevent to show a difficulty that doesn't exists (yeah i know that more than someone is gonna hard test the bot)
+                if beatmap:
                     logger.info(f"Sending map info:  MAP ID: {beatmap.id} | CHANNEL: {message.channel.id}")
                     try:
                         await message.reply(embed=map_info.create(beatmap, mapset))
                     except Exception as e:
                         logger.error(f"There was an error sending the map info: {e}")
-                except:
-                    pass
+            except:
+                pass
+        # if the link only have the mapset id it uses the highest difficulty to show up
+        # example: https://osu.ppy.sh/beatmapsets/24313
+        else:
+            try:
+                mapset = await osu.beatmapset(beatmapset_id=set_id)
+                beatmap = max(mapset.beatmaps, key=lambda b: b.difficulty_rating)
+                logger.info(f"Sending map info:  MAP ID: {beatmap.id} | CHANNEL: {message.channel.id}")
+                try:
+                    await message.reply(embed=map_info.create(beatmap, mapset))
+                except Exception as e:
+                    logger.error(f"There was an error sending the map info: {e}")
+            except:
+                pass
             
 
 def setup(bot: commands.InteractionBot):

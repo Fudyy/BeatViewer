e11e9b0c4c1fb888234ff0f830878964730fecbb
Carlos González <104286054+Fudyy@users.noreply.github.com>
2024-05-29 03:50:47 -0400

    Add: color palette image generation

--- cogs/combo_colors.py
+++ cogs/combo_colors.py
@@ -1,4 +1,5 @@
-from disnake import Attachment, Embed, ApplicationCommandInteraction as Interaction
+import io
+from disnake import Attachment, Embed, File, ApplicationCommandInteraction as Interaction
 from disnake.ext import commands
 from PIL import Image, ImageDraw
 from Pylette import extract_colors, Palette
@@ -39,6 +40,13 @@ class ComboColor(commands.Cog):
             sort_mode="frequency"
         )
 
+        palette_image = generate_palette_image(color_palette)
+        image_byte_array = io.BytesIO()
+        palette_image.save(image_byte_array, format="PNG")
+        image_byte_array.seek(0)
+        image_file = File(image_byte_array, filename="palette.png")
+        await ctx.send(embed=Embed().set_image(file=image_file))
+
 
 def setup(bot: commands.InteractionBot):
     bot.add_cog(ComboColor(bot))
@@ -48,11 +56,43 @@ def generate_palette_image(palette: Palette) -> Image.Image:
     """
     Generate an image containing the colors in the given palette.
     """
-    # Create a new image with discord dark theme background color
-    img = Image.new("RGB", (1000, 600), (43, 45, 49))
+    image_width = 1000
+    image_height = 532
+
+    palette_size = len(palette.colors)
+
+    img = Image.new("RGB", (1000, 532), (255, 255, 255))
     draw = ImageDraw.Draw(img)
 
-    # TODO: Draw the colors in the palette on the image
+    # Draw the colors in the palette on the image
+
+    if palette_size <= 5:
+        color_width = image_width // palette_size
+        color_height = image_height
+
+        for i, color in enumerate(palette):
+            draw.rectangle(
+                [(i * color_width, 0), ((i + 1) * color_width, color_height)],
+                fill=color.rgb
+            )
+    else:
+        top_color_width = image_width // 5
+        bottom_color_width = image_width // (palette_size - 5)
+
+        color_height = image_height // 2
+
+        for i, color in enumerate(palette):
+            if i < 5:
+                draw.rectangle(
+                    [(i * top_color_width, 0), ((i + 1) * top_color_width, color_height)],
+                    fill=color.rgb
+                )
+            else:
+                draw.rectangle(
+                    [((i - 5) * bottom_color_width, color_height), ((i - 4) * bottom_color_width, color_height * 2)],
+                    fill=color.rgb
+                )
+
     # TODO: Implement hitcircle generation
 
     return img
@@ -126,3 +166,4 @@ def generate_number(combo_number: int, base_image: Image.Image) -> Image.Image:
         base_image.paste(number_0, (im2_x + 15, im2_y-1), mask=number_0)
 
         return base_image
+    

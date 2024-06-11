74884b28202d1be21d8fe255a9f9d0e5e2b49d9d
Carlos Gonzalez <carlosgonsalazar@gmail.com>
2024-06-11 01:22:14 -0400

    Add: wip mapper embed

--- cogs/mapper_info.py
+++ cogs/mapper_info.py
@@ -1,6 +1,8 @@
 from disnake.ext import commands
-from disnake import ApplicationCommandInteraction as Interaction
+from disnake import UserFlags, Embed, ApplicationCommandInteraction as Interaction
+from ossapi import User
 from main import osu
+from unicodedata import lookup
 
 class MapperInfo(commands.Cog):
     def __init__(self, bot):
@@ -14,7 +16,28 @@ class MapperInfo(commands.Cog):
         except:
             return await inter.response.send_message(f"User {username} not found.", ephemeral=True)
         
-        await inter.response.send_message(f"Getting information about {user.username}...")
+        await inter.response.send_message(embed=generate_embed(user))
 
 def setup(bot):
     bot.add_cog(MapperInfo(bot))
+
+def generate_embed(user: User):
+
+    flag_unicode = [chr(0x1F1E6 + ord(char) - ord('A')) for char in user.country_code]
+
+    embed = Embed(
+        title = f"{''.join(flag_unicode)} {user.username}",
+        url = f"https://osu.ppy.sh/users/{user.id}",
+        description = f"""
+        **Subs:** {user.mapping_follower_count} | **Kudosu:** {user.kudosu.total}
+        """
+    )
+
+    embed.set_thumbnail(user.avatar_url)
+    embed.set_image(user.cover_url)
+
+    embed.set_author(name="Mapping profile for:")
+    
+    
+
+    return embed
